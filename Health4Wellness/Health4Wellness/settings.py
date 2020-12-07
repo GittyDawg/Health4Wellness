780ca1c421c77da971821f26d9cd3f54d2621e4e
brodis <brodis@andrews.edu>
2020-12-07 16:13:44 -0700

    added food model(carbs,fat,protein,calories,ingredients) and migrated the data to create it in the database

--- Health4Wellness/Health4Wellness/settings.py
+++ Health4Wellness/Health4Wellness/settings.py
@@ -29,8 +29,8 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
+    'food.apps.FoodConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
