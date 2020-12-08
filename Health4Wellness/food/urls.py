bd1c6ccb1292f1d847557d00dd197bf2cff1abb7
brodis <brodis@andrews.edu>
2020-12-07 17:46:17 -0700

    Added basic compare_search page, needs front end code

--- Health4Wellness/food/urls.py
+++ Health4Wellness/food/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<name>/', views.detail, name='detail'),
-    path('compare/<name1>/<name2>/', views.compare, name='detail'),
+    path('compare/', views.compare_search, name='compare'),
+    path('compare/<name1>/<name2>/', views.compare, name='compare'),
 ]
