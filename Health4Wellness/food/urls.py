c6ed8e5dc13faa8ecacbebad4ba365f9fc54630e
brodis <brodis@andrews.edu>
2020-12-07 20:19:13 -0700

    added searching feature to default list

--- Health4Wellness/food/urls.py
+++ Health4Wellness/food/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('compare_search/', views.compare_search, name='compare_search'),
+    path('search/', views.search, name='search'),
     path('compare/<name1>/<name2>/', views.compare, name='compare'),
     path('<name>/', views.detail, name='detail'),
 ]
